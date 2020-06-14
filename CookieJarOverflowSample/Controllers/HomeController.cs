ff20c26a586067639e942b8681122a4fa6424a58
Alexej Sommer <asommer@yandex.ru>
2020-06-14 16:25:04 +0300

    Cookie Jar Overflow Sample project added

---
+++ CookieJarOverflowSample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CookieJarOverflowSample.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace CookieJarOverflowSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            CookieOptions option = new CookieOptions();
+            option.HttpOnly = true;
+            Response.Cookies.Append("role", "user", option);
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
