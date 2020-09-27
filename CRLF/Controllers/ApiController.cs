7571e74c86c2997b8dd1c29f498c3558fdd94344
Alexej Sommer <asommer@yandex.ru>
2020-09-27 15:38:21 +0300

    OpenAPIAnalyzers are added to CRLF project

--- CRLF/Controllers/ApiController.cs
+++ CRLF/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace CRLF.Controllers
 {
@@ -15,17 +16,22 @@ namespace CRLF.Controllers
         }
 
 
-        // CRLF injection example
-        // https://owasp.org/www-community/vulnerabilities/CRLF_Injection
-        // https://localhost:44340/api/getok?param=123%0d%0aCRLF.Controllers.AdminController:%20Information:%20555
-        // simple solution - param.Replace(System.Environment.NewLine, "");
+        /// <summary>
+        /// CRLF injection example
+        /// https://owasp.org/www-community/vulnerabilities/CRLF_Injection
+        /// https://localhost:44340/api/getok?param=123%0d%0aCRLF.Controllers.AdminController:%20Information:%20555
+        /// simple solution - param.Replace(System.Environment.NewLine, "");
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         public IActionResult GetOk(string param)
         {
             _logger.LogInformation(param);
 
             return Ok("Ok");
         }
-
     }
 }
