15858b80d492fa1d148cbd496b3b5cab4ba28cf4
Alexej Sommer <asommer@yandex.ru>
2021-09-04 17:03:57 +0300

    Code example added

--- ParameterTampering/Controllers/CatalogController.cs
+++ ParameterTampering/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ParameterTampering.Entities;
+using System.Linq;
 using System.Threading.Tasks;
 
 
@@ -23,13 +24,20 @@ namespace ParameterTampering.Controllers
 
         // It is possible to send POST or edit hidden field in HTML
         // https://localhost:44335/catalog/EditArticle?Id=1&Quantity=100&Price=1.00&Name=Lego
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditArticle(/*[Bind("Id", "Name", "Quantity")]*/ Article article)
         {
             // Never trust to parameters
-            // Get object from db and map preperties
+            //var existingArticle = _context.Articles.FirstOrDefault(a => a.Id == article.Id);
+            //if (existingArticle != default)
+            //{
+            //    existingArticle.Name = article.Name;
+            //    existingArticle.Quantity = article.Quantity;
+            //    _context.Articles.Update(existingArticle);
+            //    await _context.SaveChangesAsync();
+            //}
 
             _context.Articles.Update(article);
             await _context.SaveChangesAsync();
