333245cf1834e0b6ea2ec298e53c112a6f845c7a
Alexej Sommer <asommer@yandex.ru>
2022-02-21 19:11:34 +0100

    MIME sniffing project added to solution

---
+++ MIMESniffing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIMESniffing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
